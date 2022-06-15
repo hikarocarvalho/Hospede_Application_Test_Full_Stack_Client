7d08c5fe6b6d6d91fdec578578debc5ed60f0993
hikarocarvalho <hikarofcarvalho@gmail.com>
2022-06-15 09:26:29 -0300

    feat(UserRegister): create formulary for user register

--- src/pages/userRegister/UserRegister.js
+++ src/pages/userRegister/UserRegister.js
@@ -1,9 +1,16 @@
+import Form from './../../components/form/Form';
+import Input from './../../components/input/Input';
+import Button from './../../components/button/Button';
 import './UserRegister.css';
 
 export default function UserRegister (){
     return (
-        <>
-        register
-        </>
+        <section className="user_register">
+            <Form formTitle="New User">
+                <Input id="inputUserName" description="User Name"></Input>
+                <Input id="inputUserPassword" description="User Password"></Input>
+                <Button value="Register User" />
+            </Form>
+        </section>
     );
 }
