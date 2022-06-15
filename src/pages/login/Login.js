7ae5b3dad668f4326af4b902653bf12f93432b93
hikarocarvalho <hikarofcarvalho@gmail.com>
2022-06-15 09:27:02 -0300

    feat(Login): create formulary for user login

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,9 +1,16 @@
+import Form from './../../components/form/Form';
+import Input from './../../components/input/Input';
+import Button from './../../components/button/Button';
 import './Login.css';
 
 export default function Login (){
     return (
-        <>
-            login
-        </>
+        <section className="login">
+            <Form formTitle="User Login">
+                <Input id="inputUserName" description="user name"></Input>
+                <Input id="inputUserPassword" description="user password" ></Input>
+                <Button value="Login" />
+            </Form>
+        </section>
     );
 }
