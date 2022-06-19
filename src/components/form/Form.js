597ba818223dd4964615d13324455ea0b57b9cbb
hikarocarvalho <hikarofcarvalho@gmail.com>
2022-06-19 09:42:17 -0300

    feat(Form): add legend to Form fieldset component

--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -3,7 +3,8 @@ import './Form.css';
 export default function Form(props) {
     return (
         <form className="form">
-            <fieldset className="fieldset">
+            <fieldset className="fieldset_form">
+                <legend className="form_title">{props.formTitle}</legend>
                 {props.children}
             </fieldset>
         </form>
