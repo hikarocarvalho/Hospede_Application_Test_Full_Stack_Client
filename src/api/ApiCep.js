ca7cf979202d2124c893e452312563549126e303
hikarocarvalho <hikarofcarvalho@gmail.com>
2022-06-19 09:37:57 -0300

    feat(ViaCep): create api request viacep

---
+++ src/api/ApiCep.js
@@ -0,0 +1,8 @@
+const ApiCep = async (postalCode) => {
+    let url = "https://viacep.com.br/ws/"+postalCode+"/json/";
+	const response = await fetch(url,{method:"GET"});
+	const result = await response.json();
+	return result;
+}
+
+export default ApiCep;
---
+++ src/scripts/SetAddress.js
@@ -0,0 +1,14 @@
+import ApiCep from "../api/ApiCep";
+
+const SetAddress = (eventInputs)=>{
+    ApiCep(eventInputs.inputPostCode.value).then(addressData => {
+        console.log(addressData)
+        eventInputs.inputStreet.value = addressData.logradouro;
+        eventInputs.inputComplement.value = addressData.complemento;
+        eventInputs.inputProvince.value = addressData.bairro;
+        eventInputs.inputCity.value = addressData.localidade;
+        eventInputs.inputState.value = addressData.uf;
+    });
+}
+
+export default SetAddress;
