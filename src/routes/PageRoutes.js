d7deea9e3eb4c43d76e6866e2fd1a51b5bec0613
hikarocarvalho <hikarofcarvalho@gmail.com>
2022-06-26 06:28:54 -0300

    feat(PageRoutes): create page routes

--- src/routes/PageRoutes.js
+++ src/routes/PageRoutes.js
@@ -1,4 +1,5 @@
 import { Routes, Route } from "react-router-dom";
+import AuthPage from "../components/authPage/AuthPage";
 import Home from "../pages/home/Home";
 import Login from "../pages/login/Login";
 import UserRegister from "../pages/userRegister/UserRegister";
@@ -6,7 +7,7 @@ import UserRegister from "../pages/userRegister/UserRegister";
 export default function PageRoutes (){
     return (
         <Routes>
-            <Route exact path="/" element={<Home/>}></Route>
+            <Route exact path="/" element={<AuthPage><Home/></AuthPage>}></Route>
             <Route exact path="/login" element={<Login/>}></Route>
             <Route exact path="/user-register" element={<UserRegister/>}></Route>
         </Routes>
