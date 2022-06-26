b39f28945e8d9be24aa4336683ab8a8053e67098
hikarocarvalho <hikarofcarvalho@gmail.com>
2022-06-26 06:23:27 -0300

    feat( EditUser) : create editUser component

---
+++ src/components/editUser/EditUser.js
@@ -0,0 +1,40 @@
+import Form from '../form/Form';
+import Input from '../input/Input';
+import Button from '../button/Button';
+import './EditUser.css';
+import { useEffect, useState } from 'react';
+
+export default function EditUser(props) {
+    const [userEdit, setUserEdit] = useState();
+    
+    const saveEditUserValues = (event)=>{
+        event.preventDefault();
+        let newUserData = userEdit.user;
+        newUserData.userName = event.target.inputUserName.value;
+        newUserData.email = event.target.inputEmail.value;
+        newUserData.password = event.target.inputUserPassword.value;
+        props.updateUser(newUserData);
+    }
+
+    useEffect(()=>{
+        if(!userEdit || JSON.stringify(props.userData) !== JSON.stringify(userEdit.user)){
+            setUserEdit({
+                ...userEdit,
+                user: props.userData
+            });
+        }
+    },[])
+
+    return (
+        <div className='edit_user'>  
+            {userEdit?
+                <Form formTitle="Edit User" onSubmit={saveEditUserValues}>
+                    <Input id="inputUserName" description="User Name" value={userEdit.user.userName} />
+                    <Input id="inputEmail" description="Email" value={userEdit.user.email} />
+                    <Input id="inputUserPassword" description="User Password" value={"****"} />
+                    <Button value="Edit User" />
+                </Form>
+            :null}
+        </div>
+    );
+}
