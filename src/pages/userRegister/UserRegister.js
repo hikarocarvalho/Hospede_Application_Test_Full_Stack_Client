a50d47013d543a2825b8d821305301eb5e07c091
hikarocarvalho <hikarofcarvalho@gmail.com>
2022-06-26 06:28:13 -0300

    feat(UserRegister): create user register functions

--- src/pages/userRegister/UserRegister.js
+++ src/pages/userRegister/UserRegister.js
@@ -1,46 +1,40 @@
 import Form from './../../components/form/Form';
 import Input from './../../components/input/Input';
 import Button from './../../components/button/Button';
-import Fieldset from '../../components/fieldSet/Fieldset';
-import SetAddress from '../../scripts/SetAddress';
+import User from '../../api/entities/User';
 import './UserRegister.css';
 import { useEffect } from 'react';
 
 export default function UserRegister (){
 
-    const searchPostalCode = (event) => {
-        event.preventDefault();
-        if(event.target.value.length === 8){
-            SetAddress(event.target.parentElement.children);
-        }
+    const preload = {
+        userName: "",
+        email: "",
+        password: "",
     }
+
+    const createUser = (event) => {
+        event.preventDefault();
+        console.log("teste")
+
+        preload.userName = event.target.inputUserName.value;
+        preload.email = event.target.inputEmail.value;
+        preload.password = event.target.inputUserPassword.value;
+
+        User.createUser(preload);
+    };
 
     useEffect(()=>{
 
-    },[])
+    },[]);
 
     return (
         <section className="user_register">
-            <Form formTitle="New User">
-                <Fieldset fieldsetTitle="Personal Data">
-                    <Input id="inputCompleteName" description="Complete Name"></Input>
-                    <Input id="inputBirthDate" description="Birth Date"></Input>
-                    <Input id="inputCpf" description="Cpf"></Input>
-                </Fieldset>
-                <Fieldset fieldsetTitle="User Data">
-                    <Input id="inputEmail" description="Email"></Input>
-                    <Input id="inputUserName" description="User Name"></Input>
-                    <Input id="inputUserPassword" description="User Password"></Input>
-                </Fieldset>
-                <Fieldset fieldsetTitle="Address Data">
-                    <Input id="inputPostCode" description="Post Code" onChange={searchPostalCode}></Input>
-                    <Input id="inputStreet" description="Street"></Input>
-                    <Input id="inputComplement" description="Complement"></Input>
-                    <Input id="inputProvince" description="Province"></Input>
-                    <Input id="inputCity" description="City"></Input>
-                    <Input id="inputState" description="State"></Input>
-                    <Button value="Register User" />
-                </Fieldset>
+            <Form formTitle="New User" onSubmit={createUser}>
+                <Input id="inputUserName" description="User Name" />
+                <Input id="inputEmail" description="Email" />
+                <Input id="inputUserPassword" description="User Password" />
+                <Button value="Register User" />
             </Form>
         </section>
     );
