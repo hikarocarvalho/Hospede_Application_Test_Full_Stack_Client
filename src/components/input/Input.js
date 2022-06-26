5c11756cbf912a6c359aa0dfd3f683cec84d5b9c
hikarocarvalho <hikarofcarvalho@gmail.com>
2022-06-26 06:24:40 -0300

    feat( Input ): define input configs

--- src/components/input/Input.js
+++ src/components/input/Input.js
@@ -4,7 +4,13 @@ export default function Input (props){
     return (
         <>
             <label htmlFor={props.id} className="input_title" >{props.description}</label>
-            <input id={props.id} name={props.id} className="input" onChange={props.onChange?props.onChange:null}/>
+            <input 
+                id={props.id} 
+                name={props.id} 
+                className="input" 
+                onChange={props.onChange?props.onChange:null} 
+                defaultValue={props.value?props.value:undefined}
+            />
         </>
     );
 }
