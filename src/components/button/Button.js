30c7268722452b0b8fa24082cfe0d6b4283b0e7b
hikarocarvalho <hikarofcarvalho@gmail.com>
2022-06-26 06:22:53 -0300

    feat( Button): remove event prevent from default

--- src/components/button/Button.js
+++ src/components/button/Button.js
@@ -1,12 +1,8 @@
 import './Button.css';
 
 export default function Button (props){
-
-    const preventSubmit = (event) => {
-        event.preventDefault();
-    }
     return (
-        <button className="button" onClick={preventSubmit}>
+        <button className="button">
             {props.value}
         </button>
     );
