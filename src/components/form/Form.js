6bdf5f2ccb1c9fcdd58741a0d1fb97da971cf59d
hikarocarvalho <hikarofcarvalho@gmail.com>
2022-06-26 06:24:09 -0300

    feat(Form): add onSubmit condition for form component

--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -2,7 +2,7 @@ import './Form.css';
 
 export default function Form(props) {
     return (
-        <form className="form">
+        <form className="form" onSubmit={props.onSubmit?props.onSubmit:null}>
             <fieldset className="fieldset_form">
                 <legend className="form_title">{props.formTitle}</legend>
                 {props.children}
